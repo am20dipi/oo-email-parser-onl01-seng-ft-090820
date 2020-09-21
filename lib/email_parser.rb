8dc8f962156aee8497ed0bfa285454917759db20
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-21 18:37:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,7 @@ class EmailAddressParser
   attr_accessor :email_addresses
   
   def initialize(email_addresses)
-    email_addresses = EmailAddressParser.new
+    @email_addresses = EmailAddressParser.new(email_adresses)
     email_adresses.parse.uniq
   end
 end
